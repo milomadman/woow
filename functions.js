80b27dd0db00cfce440d08f30e75d6d075ec9e37
milomadman <128404660+milomadman@users.noreply.github.com>
2024-07-01 10:53:01 +0200

    Add files via upload

--- functions.js
+++ functions.js
@@ -4,35 +4,50 @@ function getSVGString(img) {
     return svgString;
 }
 
-addEventListener("load", () => {
-    colors = ["#ff0000","#1EAFC7","#ffa500","#008000"];
+/*addEventListener("load", () => {
+    colors = ["#ff0000","#1eafc7","#ffa500","#008000","#ff00c5","#ab6cf2"];
 
     x = Math.floor(Math.random()*colors.length);
     mycolor = colors[x]
 
     document.getElementsByClassName("active")[0].style.backgroundColor=mycolor;
 
-    //I want the logo to change color as well
-    const logo = document.querySelector('.logo img');    
+    const paths = document.querySelectorAll('#logo-svg path');
+    paths.forEach(path => {
+        path.setAttribute('fill', mycolor); // Change to your desired color
+    });
 
-    // Create a new Image object
-    const img = new Image();
-    img.src = logo.src;
+});*/
 
-    // When the image is loaded, handle the SVG manipulation
-    img.onload = function() {
-        const svgString = getSVGString(img);
-        const parser = new DOMParser();
-        const svgDoc = parser.parseFromString(svgString, 'image/svg+xml');
-        
-        // Update the fill color of all paths in the SVG
-        const paths = svgDoc.querySelectorAll('path');
-        paths.forEach(path => {
-            path.setAttribute('fill', mycolor);
-        });
 
-        // Serialize the updated SVG back to a string and replace the original <img> tag
-        const updatedSvgString = new XMLSerializer().serializeToString(svgDoc.documentElement);
-        logo.outerHTML = updatedSvgString;
-    };
+document.addEventListener('DOMContentLoaded', function () {
+    colors = ["#ff0000","#1eafc7","#ffa500","#008000","#ff00c5","#ab6cf2"];
+
+    x = Math.floor(Math.random()*colors.length);
+    mycolor = colors[x]
+
+    document.getElementsByClassName("active")[0].style.backgroundColor=mycolor;
+
+    var src;
+    if (window.location.pathname == "/index.html"){
+        src = 'logo-wide.svg'
+    } else {
+        src = '../logo-wide.svg'
+    }
+    
+    fetch(src)
+        .then(response => response.text())
+        .then(svg => {
+            container = document.getElementById('logo-container')
+            container.innerHTML = svg;
+
+            const svgElement = container.querySelector('svg');
+            svgElement.setAttribute('height', '100%');
+
+            const paths = document.querySelectorAll('#logo-svg path');
+            paths.forEach(path => {
+            path.setAttribute('fill', mycolor); // Change to your desired color
+    });
+        })
+        .catch(error => console.error('Error loading SVG:', error));
 });
